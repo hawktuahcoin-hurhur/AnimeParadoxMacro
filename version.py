cca42980c4fbb8bbe7675a0670d399c02c7ff1f6
HDev <harrisonewalther@gmail.com>
2026-02-01 22:36:16 -0500

    Fix version to 0.0.0 and update GitHub URLs to main branch

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """Version information for AnimeParadoxMacro"""
 
-VERSION = "1.0.0"
+VERSION = "0.0.0"
 GITHUB_REPO = "hawktuahcoin-hurhur/AnimeParadoxMacro"
-GITHUB_RAW_URL = f"https://raw.githubusercontent.com/{GITHUB_REPO}/master"
+GITHUB_RAW_URL = f"https://raw.githubusercontent.com/{GITHUB_REPO}/main"
 GITHUB_RELEASES_API = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
